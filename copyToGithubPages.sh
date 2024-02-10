379bb3f17615e6dbe94991bcae65d52d38cca1b2
Vincent VIVANLOC <vincent.vivanloc@laposte.net>
2024-02-11 00:41:23 +0100

    bomp

--- copyToGithubPages.sh
+++ copyToGithubPages.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # dedicated to githubpages, to not fiddle with _next
 #cloneRepoPath=/userdata/dev/tdv_site/toulousedataviz.github.io
+srcDir=docs
+destDir=docs
 mkdir -p docs
 cloneRepoPath=docs
-touch out/.nojekyll
-cp -R out/* $cloneRepoPath
+
+if [ "$srcDir" != "$destDir" ] 
+then
+    cp -R out/* $cloneRepoPath
+fi
+touch $cloneRepoPath/.nojekyll
 cp $cloneRepoPath/index.html $cloneRepoPath/guide.html 
