34350732552e95c843333cececd273b387c7685e
Vincent VIVANLOC <vincent.vivanloc@laposte.net>
2024-02-23 22:25:00 +0100

    bump

--- copyToGithubPages.sh
+++ copyToGithubPages.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
-# dedicated to githubpages, to not fiddle with _next
-#cloneRepoPath=/userdata/dev/tdv_site/toulousedataviz.github.io
+# dedicated to githubpages, to not fiddle with _next Vercel
+
 srcDir=docs
 destDir=docs
-mkdir -p docs
-cloneRepoPath=docs
+
+if [ "$1" == "--prod" ]
+then
+    # local folder where github page repo is
+    destDir=/userdata/dev/tdv_site/toulousedataviz.github.io/guide
+fi
+mkdir -p $destDir
+
+echo "Creating index html page"
+cp $srcDir/index.html $srcDir/guide.html 
 
 if [ "$srcDir" != "$destDir" ] 
 then
-    cp -R out/* $cloneRepoPath
+    echo "Copying to $destDir..."    
+    cp -Rfv $srcDir/* $destDir
 fi
-touch $cloneRepoPath/.nojekyll
-cp $cloneRepoPath/index.html $cloneRepoPath/guide.html 
+
+# for github pages 
+touch $destDir/.nojekyll
+
+
